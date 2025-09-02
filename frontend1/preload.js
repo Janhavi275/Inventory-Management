98da4ed71b7715703384d0dc1ac7374b22cb23b1
Janhavi275 <sonarjanu2005@gmail.com>
2025-09-02 11:01:32 +0530

    Not useful, following update frontend and backend

--- frontend1/preload.js
+++
@@ -1,7 +0,0 @@
-// frontend/preload.js
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  openExternal: (url) => ipcRenderer.invoke("open-external", url),
-  // Add more IPC methods if you need native features later
-});

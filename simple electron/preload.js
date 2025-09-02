7278de86c7ec629fe6b461c4ae2046332d790513
Janhavi275 <sonarjanu2005@gmail.com>
2025-09-02 11:03:37 +0530

    This file having frontend and backend combine and only limited to login

--- simple electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  login: (email, password, role) => ipcRenderer.invoke('login', { email, password, role }),
-  navigate: (page) => ipcRenderer.send('navigate', page)
-});
